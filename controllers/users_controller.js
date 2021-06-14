8f1daed4c309994ce533e07c1e8754d501d44011
Rajat Uba <rajatuba9783@gmail.com>
2021-06-15 02:20:59 +0530

    Rendering pages for Sign in and Sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile=function(req,res){
         title:'Profile'
     });
 }
+
+//render sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: "ChattingSite | Sign Up"
+    });
+}
+
+//render sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"ChattingSite | Sign In"
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,8 @@ const usersController= require('../controllers/users_controller');
 
 router.get('/profile',usersController.profile);
 
+//routes for sign in and sign up
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
 module.exports=router;
