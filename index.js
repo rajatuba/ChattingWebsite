3166dc8dab5abb23f49d1744eb111e287de5db7a
Rajat Uba <rajatuba9783@gmail.com>
2021-06-15 01:41:42 +0530

    Creating and setting up layouts"

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//for creating layouts
+const expressLayouts=require('express-ejs-layouts');
+app.use(expressLayouts);
+
 //use express router
 app.use('/',require('./routes'));
 
