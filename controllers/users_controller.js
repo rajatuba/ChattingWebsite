6a2f8812a3808a419717ef20720cd25042a2805d
Rajat Uba <rajatuba9783@gmail.com>
2021-06-15 03:02:42 +0530

    Router and controller  for user sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+//importing user model
+const User=require('../models/user')
+
 module.exports.profile=function(req,res){
     res.render('user_profile',{
         title:'Profile'
@@ -15,5 +18,29 @@ module.exports.signUp=function(req,res){
 module.exports.signIn=function(req,res){
     return res.render('user_sign_in',{
         title:"ChattingSite | Sign In"
+    });
+}
+
+//get the sign up data
+module.exports.create=function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
