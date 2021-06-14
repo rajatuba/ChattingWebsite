63f0837c14d3b7aa92033513a308006e9a14fe46
Rajat Uba <rajatuba9783@gmail.com>
2021-06-15 01:56:30 +0530

    Extraction of scripts and styles of subpages and adding more static files

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const expressLayouts=require('express-ejs-layouts');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
+//extract style and script from sub-pages into layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 //use express router
 app.use('/',require('./routes'));
 
