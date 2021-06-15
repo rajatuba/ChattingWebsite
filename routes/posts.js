8b3b717f8a30bcb77c491b8a3589896ac192cead
Rajat Uba <rajatuba9783@gmail.com>
2021-06-15 23:41:22 +0530

    Authentication on Post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router=express.Router();
+//for authenication
+const passport = require('passport');
+
 const postsController=require('../controllers/posts_controller');
 
-router.post('/create',postsController.create);
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 module.exports=router;
